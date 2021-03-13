aa06ace2d47d31bf37b78e27130045c725e06234
dj <djamesfar@gmail.com>
2021-03-12 16:16:06 -0800

    fixed migration classname

--- database/migrations/2021_03_11_110000_add_profiles_permissions.php
+++ database/migrations/2021_03_11_110000_add_profiles_permissions.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Tipoff\Authorization\Permissions\BasePermissionsMigration;
 
-class AddProfilePermissions extends BasePermissionsMigration
+class AddProfilesPermissions extends BasePermissionsMigration
 {
     public function up()
     {
