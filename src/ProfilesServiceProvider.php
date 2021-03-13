30ee07ea6dd3343370cee16281b81f8d34175eac
dj <djamesfar@gmail.com>
2021-03-12 17:13:10 -0800

    add providers

--- src/ProfilesServiceProvider.php
+++ src/ProfilesServiceProvider.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Tipoff\Profiles;
 
+use Tipoff\Profiles\Models\Profile;
+use Tipoff\Profiles\Models\ProfileLink;
+use Tipoff\Profiles\Policies\ProfileLinkPolicy;
+use Tipoff\Profiles\Policies\ProfilePolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -12,8 +16,15 @@ class ProfilesServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
+            ->hasPolicies([
+                Profile::class => ProfilePolicy::class,
+                ProfileLink::class => ProfileLinkPolicy::class,
+            ])
+            ->hasNovaResources([
+                \Tipoff\Profiles\Nova\Profile::class,
+                \Tipoff\Profiles\Nova\ProfileLink::class,
+            ])
             ->name('profiles')
-            ->hasViews()
             ->hasConfigFile();
     }
 }
